2f64114d972ce0e30a2da070dffa6318dea65fd0
alfredoT7 <123456alfredotorri@gmail.com>
2024-09-16 21:20:27 -0400

    feat: Second secition finished, cards component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,33 +4,51 @@ import NavBar from '../../components/navbar/NavBar'
 import imgPrimary from '../../assets/img-primary.jpg'
 import imgSecondary from '../../assets/img-secondary.jpg'
 import imgTerniary from '../../assets/img-terniary.jpg'
+import Card from '../../components/cards/Card'
+import imgComunicado from '../../assets/image-card-comunicate.jpg'
 function Home() {
   return (
     <section>
         <NavBar/>
-        <div className='main-container'>
-            <div className='content'>
-                <p className='tittle'>Centro de estudiantes de ingenieria de sistemas</p>
-                <p className='paragraph'>we made it so breautiful and simple. It combines landings, pages, blogs and shop screens. It is defenitly the tool you need in your collection!</p>
-            </div>
-            <div className='content2'>
-                
-                <div className='image-vision'>
-                    <div className="principal-img">
-                        <img src={imgPrimary} alt="" />
-                    </div>
-
-                    <div className="secundary-img">
-                        <img src={imgSecondary} alt="" />
-                    </div>
-
-                    <div className="terniary-img">
-                        <img src={imgTerniary} alt="" />
-                    </div>
+        <main >
+            <div className='main-container'>
+                <div className='content'>
+                    <p className='tittle'>Centro de estudiantes de ingenieria de sistemas</p>
+                    <p className='paragraph'>
+                        we made it so breautiful and simple. It combines landings, pages, blogs and shop screens. It is defenitly the tool you need in your collection!
+                    </p>
                 </div>
+                <div className='content2'>
+                    
+                    <div className='image-vision'>
+                        <div className="principal-img">
+                            <img src={imgPrimary} alt="" />
+                        </div>
+
+                        <div className="secundary-img">
+                            <img src={imgSecondary} alt="" />
+                        </div>
+
+                        <div className="terniary-img">
+                            <img src={imgTerniary} alt="" />
+                        </div>
+                    </div>
                 
+                </div>
             </div>
-        </div>
+        </main>
+
+        <main className='main-redirect-container'>
+            <p className='tittle'>Title</p>
+            <div className='card-container'>
+                <Card className="custom-card" image={imgComunicado} title="Comunicados" description="¡Atención! Consulta los últimos comunicados del Centro de Estudiantes de Ingeniería de Sistemas para estar al día con eventos y noticias importantes."  />
+                <Card className="custom-card" image={imgPrimary} title="Cursos" description="¡Atención! Consulta los últimos comunicados del Centro de Estudiantes de Ingeniería de Sistemas para estar al día con eventos y noticias importantes."  />
+                <Card className="custom-card" image={imgPrimary} title="Recursos" description="¡Atención! Consulta los últimos comunicados del Centro de Estudiantes de Ingeniería de Sistemas para estar al día con eventos y noticias importantes."  />
+            </div>
+        </main>
+
+
+
     </section>
 );
 }
