5b30b3b85a0948bb26d124ad0e979281c258aaea
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2024-09-18 06:11:00 -0400

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import NavBar from './components/navbar/NavBar';
 import Home from './pages/home/Home';
+import OurCareer from './pages/ourcareer/OurCareer';
+import Resource from './pages/resource/Resource';
 function App() {
     return(
         <Router>
+        <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/nuestra-carrera" element={<OurCareer />} />
+                <Route path="/recursos" element={<Resource />} />
             </Routes>
         </Router>
         
---
+++ src/assets/ImagesApp.js
@@ -0,0 +1,5 @@
+import ImgPrimay from './img-primary.jpg'
+
+export default {
+    "ImgPrimay" : ImgPrimay
+}
